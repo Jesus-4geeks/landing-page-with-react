25ea653d9defeeb6c9b29fb4b449ec5afa378995
Jesus-4Geeks <madafaking@PC-JML>
2025-04-27 20:17:16 +0200

    Added home (main content)

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,24 +1,16 @@
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Jumbotron from "./Jumbotron";
+import Card from "./Card";
 
-//create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div id="home" className="container-fluid px-5 py-4">
+			<Jumbotron />
+			<div className="row px-2">
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+			</div>
 		</div>
 	);
 };
