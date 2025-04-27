e163e25bff38ba552b116a122eaf7c2a9fce3c2c
Jesus-4Geeks <madafaking@PC-JML>
2025-04-27 18:44:12 +0200

    Added footer

---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <div className="navbar bg-dark p-5">
+            <div className="container-fluid justify-content-center text-white">
+                Copyright &copy; My Website 2025
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
