b1ca066d10f6df5fda665dc6a1cb38d70df2b080
Jesus-4Geeks <madafaking@PC-JML>
2025-04-27 20:06:14 +0200

    Added jumbotron

---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,15 @@
+const Jumbotron = () => {
+    return (
+        <div className="p-5 mb-4 bg-body-secondary rounded-3">
+            <div className="container-fluid py-5">
+                <h1 className="display-5 fw-normal">A Warm Welcome!</h1>
+                <p className="col-md-8 fs-5">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos placeat dolore, reiciendis repellendus est quis perferendis! Deleniti voluptates debitis cumque mollitia corporis rem rerum. Illo laudantium rerum eius odio quis?
+                </p>
+                <button className="btn btn-primary btn-lg" type="button">Call to action!</button>
+            </div>
+        </div>
+    );
+};
+
+export default Jumbotron;
